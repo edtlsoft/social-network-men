dba2a5be4fd0ef62eb2feec6dc763bb549f651a6
Edtlsoft <edwardlopez2824@gmail.com>
2020-12-12 21:26:06 -0500

    [add] Saving images in MongoDB

--- src/Controllers/image.js
+++ src/Controllers/image.js
@@ -2,25 +2,46 @@ const path = require('path')
 
 const fs = require('fs-extra')
 
+const { Image } = require('../Models')
+
 const { randomNameImage } = require('../Helpers/lib')
 
 module.exports = {
     index: (req, res) => {
         res.send(`Mostrando la imagen con id numero: ${req.params.image_id}`)
     },
-    create: async (req, res) => {
-        const nameImage = randomNameImage('imagen-post-')
-        const ext = req.file.originalname.split('.').pop().toString().toLowerCase()
-        const imageTempPath = req.file.path
-        const targetPath = path.resolve(`src/Public/upload/${nameImage}.${ext}`)
-        const validatedExtensions = ['png', 'jpg', 'jpeg']
-        console.log(ext, nameImage, imageTempPath, targetPath)
+    create: (req, res) => {
+        const saveImage = async () => {
+            const nameImage = randomNameImage('imagen-post-')
+            const images = await Image.find({filename: nameImage})
 
-        if( validatedExtensions.includes(ext) ) {
-            await fs.rename(imageTempPath, targetPath)
+            if( images.length ) {
+                saveImage()
+            }
+
+            const ext = req.file.originalname.split('.').pop().toString().toLowerCase()
+            const imageTempPath = req.file.path
+            const targetPath = path.resolve(`src/Public/upload/${nameImage}.${ext}`)
+            const validatedExtensions = ['png', 'jpg', 'jpeg']
+
+            if( validatedExtensions.includes(ext) ) {
+                await fs.rename(imageTempPath, targetPath)
+                const image = new Image({
+                    title: req.params.title,
+                    description: req.params.description,
+                    filename: `${nameImage}.${ext}`
+                })
+                const imageSave = await image.save()
+                // req.redirect('/images/')
+                res.send('Working...')
+            }
+            else {
+                await fs.unlink(imageTempPath)
+                res.status(500).json({error: 'The extension of the file is not supported'})
+            }
         }
 
-        res.send('funcionando...')
+        saveImage()
     },
     like: (req, res) => {
         res.send(`Like a la imagen con id numero: ${req.params.image_id}`)
